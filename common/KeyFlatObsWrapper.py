d2db7ef9f36a28f61879311fb5cbf722202a6102
maor hornstein <mor.h100@gmail.com>
2024-03-12 07:57:44 +0200

    First commit - with KeyFlatObsWrapper working

---
+++ common/KeyFlatObsWrapper.py
@@ -0,0 +1,116 @@
+import gym
+from gym.vector.utils import spaces
+from common.minigrid_x import OBJECT_TO_IDX, COLOR_TO_IDX
+import numpy as np
+
+max_env_steps = 100
+
+class KeyFlatObsWrapper(gym.core.ObservationWrapper):
+    """Fully observable gridworld returning a flat grid encoding."""
+
+    def __init__(self, env):
+        super().__init__(env)
+
+        # Since the outer walls are always present, we remove left, right, top, bottom walls
+        # from the observation space of the agent. There are 3 channels, but for simplicity
+        # in this assignment, we will deal with flattened version of state.
+
+        self.observation_space = spaces.Box(
+            low=0,
+            high=255,
+            shape=((self.env.width-2) * (self.env.height-2) * 3,),  # number of cells
+            dtype='uint8'
+        )
+        self.unwrapped.max_steps = max_env_steps
+
+        self.get_init_map()
+
+    def observation(self, obs):
+        # this method is called in the step() function to get the observation
+        # we provide code that gets the grid state and places the agent in it
+        env = self.unwrapped
+        full_grid = env.grid.encode()
+        full_grid[env.agent_pos[0]][env.agent_pos[1]] = np.array([
+            OBJECT_TO_IDX['agent'],
+            COLOR_TO_IDX['red'],
+            env.agent_dir
+        ])
+        full_grid = full_grid[1:-1, 1:-1]   # remove outer walls of the environment (for efficiency)
+
+        flattened_grid = full_grid.ravel()
+        return flattened_grid
+
+    def is_carrying_key(self):
+        if 'Key' in str(self.unwrapped.carrying):
+          return True
+        return False
+
+    def is_door_open(self):
+        return self.unwrapped.env_door.is_open
+
+    def get_position(self):
+        return self.unwrapped.agent_pos
+
+    def get_direction(self):
+        return self.unwrapped.agent_dir
+
+    def get_direction_str(self):
+        return self.unwrapped.AGENT_DIR_TO_STR[self.unwrapped.agent_dir]
+
+    def is_wall_front_pos(self):
+      if 0 in self.unwrapped.front_pos or (self.unwrapped.height-1) in self.unwrapped.front_pos:
+        return True
+      return (tuple(self.unwrapped.front_pos) in self.unwrapped.walls_init)
+
+    def get_goal_pos(self):
+      return self.unwrapped.goal_pos
+
+    def get_door_pos(self):
+      return self.unwrapped.door_pos
+
+    def get_key_pos(self):
+      return self.unwrapped.key_pos
+
+    def get_init_map(self):
+      # outer walls
+      map_u = [[1 if i == 0 or i == (self.unwrapped.height-1) or j == 0 or j == (self.unwrapped.width-1) else 0 for j in range(self.unwrapped.width)] for i in range(self.unwrapped.height)]
+      # inner walls
+      for loc in self.unwrapped.walls_init: map_u[loc[1]][loc[0]] = 1
+      # map_u[self.unwrapped.width-2][self.unwrapped.height-2] = 'G'
+      self.init_map = map_u
+
+    def get_updated_map(self):
+      map_up = copy.deepcopy(self.init_map)
+      # agent position
+      agent_col, agent_row = self.unwrapped.agent_pos
+      map_up[agent_row][agent_col] = 'P'
+      if not self.is_door_open():
+        door_col, door_row = self.unwrapped.door_pos
+        map_up[door_row][door_col] = 'D'
+        if not self.is_carrying_key():
+          key_col, key_row = self.unwrapped.key_pos
+          map_up[key_row][key_col] = 'K'
+      # goal
+      curr_goal_pos = self.get_goal_pos()
+      map_up[curr_goal_pos[1]][curr_goal_pos[0]] = 'G'
+
+      return map_up
+
+    def is_door_front_pos(self):
+      return (tuple(self.unwrapped.front_pos) == self.unwrapped.get_d_pos())
+
+    def is_key_front_pos(self):
+      return (tuple(self.unwrapped.front_pos) == self.unwrapped.get_k_pos())
+
+    def render(self, *args, **kwargs):
+        """This removes the default visualization of the partially observable field of view."""
+        return self.unwrapped.render()
+
+
+if __name__ == '__main__':
+    from common.RandomKeyMEnv_10 import RandomEmptyEnv_10
+    import matplotlib.pyplot as plt
+    env = KeyFlatObsWrapper(RandomEmptyEnv_10(render_mode='rgb_array'))
+    obs = env.reset()
+    plt.imshow(env.render())
+    print()
---
+++ common/RandomKeyMEnv_10.py
@@ -0,0 +1,126 @@
+import gym
+import random
+from common.minigrid_x import MissionSpace, Wall, MiniGridEnv, Door, COLOR_NAMES, Goal
+from gym_minigrid.minigrid import Grid
+from gym import spaces
+
+class RandomEmptyEnv_10(MiniGridEnv):
+    def __init__(
+        self,
+        size=10,
+        agent_start_pos=(1, 1),
+        agent_start_dir = 0,
+        max_steps: int | None = None,
+        **kwargs,
+    ):
+
+        self.agent_start_pos = agent_start_pos
+        self.agent_start_dir = agent_start_dir
+
+        agent_start_x_loc =random.randint(1, 6)
+        agent_start_y_loc =random.randint(1, 6)
+
+        self.agent_start_pos = (agent_start_x_loc,agent_start_y_loc)
+
+
+        mission_space = MissionSpace(mission_func=self._gen_mission)
+
+        if max_steps is None:
+            max_steps = 4 * size**2
+
+        super().__init__(
+            mission_space=mission_space,
+            grid_size=size,
+            # Set this to True for maximum speed
+            see_through_walls=True,
+            max_steps=max_steps,
+            **kwargs,
+        )
+        self.action_space = spaces.Discrete(3)
+        self.walls_init = []
+
+        # change 21-09 for random env
+        self.not_goal_loc = [(col,row) for col in range(self.unwrapped.width) for row in range(self.unwrapped.height) if row == 0 or row == (self.unwrapped.width-1) or col == 0 or col == (self.unwrapped.height-1)]
+        self.not_goal_loc.append(self.agent_start_pos)
+        self.goal_pos = (self.grid.width - 2, self.grid.height - 2)
+
+    def reset(self, **kwargs):
+        # Randomize start position each time environment is reset
+        agent_start_x_loc = random.randint(1, 6)
+        agent_start_y_loc = random.randint(1, 6)
+        self.agent_start_pos = (agent_start_x_loc, agent_start_y_loc)
+
+        self.values = [0, 1, 2, 3]
+        self.agent_start_dir = random.choice(self.values)
+
+        # Recalculate not_goal_loc as it depends on the agent's start position
+        self.not_goal_loc = [(col, row) for col in range(self.unwrapped.width) for row in range(self.unwrapped.height)
+                             if row == 0 or row == (self.unwrapped.width - 1) or col == 0 or col == (self.unwrapped.height - 1)]
+        self.not_goal_loc.append(self.agent_start_pos)
+
+        # Call the reset method of the parent class
+        return super().reset(**kwargs)
+
+    @staticmethod
+    def _gen_mission():
+        return "grand mission"
+
+    def get_k_pos(self):
+      return self.key_pos
+
+    def get_d_pos(self):
+      return self.door_pos
+
+    def get_g_pos(self):
+      return self.goal_pos
+
+    def _gen_grid(self, width, height):
+        # Create an empty grid
+        self.grid = Grid(width, height)
+
+        # Generate the surrounding walls
+        self.grid.wall_rect(0, 0, width, height)
+
+        # Generate verical separation wall
+        # for i in range(0, height):
+        #     self.grid.set(5, i, Wall())
+
+
+        for column, row in self.walls_init:
+          self.grid.set(column, row, Wall())
+
+        self.key_pos = (6, 5)
+        self.door_pos = (6, 7)
+
+        self.env_door = Door(COLOR_NAMES[0], is_locked=True)
+
+        # Place the Lava
+        # self.grid.set(4, 6, Lava())
+
+        # Place the door and key
+        # self.grid.set(self.door_pos[0], self.door_pos[1], self.env_door)
+        # self.grid.set(self.key_pos[0], self.key_pos[1], Key(COLOR_NAMES[0]))
+
+
+
+        ## change 21-09 for random env - add player position
+        # self.goal_pos = (0, 0)
+        # while self.goal_pos in self.not_goal_loc:
+        #   self.goal_pos = (random.randint(1, width - 2), random.randint(1, height - 2))
+        self.goal_pos = random.choice([(8,1), (8,8), (1,8)])
+
+
+        self.put_obj(Goal(), self.goal_pos[0], self.goal_pos[1])
+
+        # Place a goal square in the bottom-right corner
+        # self.put_obj(Goal(), width - 2, height - 2)
+
+
+        # Place the agent
+        if self.agent_start_pos is not None:
+            self.agent_pos = self.agent_start_pos
+            self.agent_dir = self.agent_start_dir
+        else:
+            self.place_agent()
+
+        self.mission = "grand mission"
