27955527343636fd4eb63c0892a9d0d775a373f2
maor hornstein <mor.h100@gmail.com>
2024-03-12 09:41:46 +0200

    fix get_current_state + implement get_state_dim, get_action_dim, sample_action

--- common/EmptyEnvWrapper.py
+++ common/EmptyEnvWrapper.py
@@ -3,6 +3,8 @@ import random
 import numpy as np
 import copy
 
+agent_directions_space = 4
+
 class EnvWrapper(gym.Env):
     '''
     This wrapper enables environment customization
@@ -17,13 +19,24 @@ class EnvWrapper(gym.Env):
         return self.get_current_state()
 
     def get_current_state(self):
-        agent_position = self.env.get_position()
+        agent_col, agent_row = self.env.get_position()
         agent_direction = self.env.get_direction()
-        state = agent_position + (agent_direction,)
+        state = (agent_col - 1, agent_row - 1, agent_direction) # remove extra row \ col in the board perimeter
         return state
 
     def render(self):
         plt.imshow(self.env.render())
+
+    def get_state_dim(self):
+        rows = self.env.height - 2
+        cols = self.env.width - 2
+        return cols, rows, agent_directions_space
+
+    def get_action_dim(self):
+        return self.env.action_space.n
+
+    def sample_action(self):
+        return self.env.action_space.sample()
 
 # Testing
 if __name__ == '__main__':
@@ -35,3 +48,9 @@ if __name__ == '__main__':
 
     state = env.reset()
     print(f'start_state: {state}')
+
+    print(f'states dim: {env.get_state_dim()}')
+    print(f'action dim: {env.get_action_dim()}')
+
+    action = env.sample_action()
+    print(f'sampled action: {action}')
