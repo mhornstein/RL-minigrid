26f45e43b928858e6e461cd81ddbcb0646075413
maor hornstein <mor.h100@gmail.com>
2024-03-13 10:30:32 +0200

    Reorganize config file (in order to support the key env)

--- part1/experiment_config.py
+++ part1/experiment_config.py
@@ -24,26 +24,23 @@ test_steps_cutoff - maximal steps allowed per episode
 '''
 test_steps_cutoff = 100
 
+# Default hyper-parameters values for algorithm (the tested hyperparameter will override its value in the relevant test)
+algorithm_params = {'alpha': 0.9, 'gamma': 1, 'epsilon': 1.0, 'ep_decay': 0.99,
+                    'num_episodes': train_num_episodes, 'steps_cutoff': train_steps_cutoff}
+
+###################################################
+# Tested hyperparameter configuration - Empty env #
+###################################################
+
 # Tested env
 env = KeyFlatObsWrapper(RandomEmptyEnv_10(render_mode='rgb_array'))
 env.reset() # we must reset the env: this places the agent and other elements on top of the board
 
-'''
-Default hyper-parameters values (the tested hyperparameter will override its value in the relevant test)
-'''
+# Default hyper-parameters values for env (the tested hyperparameter will override its value in the relevant test)
 env_params = {'goal_reward': 10, 'step_reward': -0.01, 'env': env}
 
-algorithm_params = {'alpha': 0.9, 'gamma': 1, 'epsilon': 1.0, 'ep_decay': 0.99,
-                    'num_episodes': train_num_episodes, 'steps_cutoff': train_steps_cutoff}
-
-#######################################
-# Tested hyperparameter configuration #
-#######################################
-
-'''
-remove dictionary keys to test less hyperparameters
-Change the values in the entries to test different hyper-parameters values
-'''
+# remove dictionary keys to test less hyperparameters
+# Change the values in the entries to test different hyper-parameters values
 tested_parameters = {'goal_reward': [-10, 0, 1, 5, 10],
                     'step_reward': np.arange(-0.006, 0.007, 0.002).round(3),
                     'alpha': np.arange(0.85, 1.01, 0.025).round(2),
